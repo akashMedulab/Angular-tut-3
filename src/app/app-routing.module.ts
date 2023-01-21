6080574c520f750e40d3e3b161479b21bb3f3274
akashMedulab <akashmourya@edulab.in>
2023-01-21 15:44:40 +0530

    child Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NoPageComponent } from './no-page/no-page.component';
@@ -12,8 +14,16 @@ const routes: Routes = [
     component:UserComponent
   },
   {
+    path:'about',
+    component: AboutComponent
+  },
+  {
     path:'about/:id',
-    component:AboutComponent
+    component:AboutComponent,
+    children:[
+      {path:'company',component:AboutCompanyComponent},
+      {path:'me',component:AboutMeComponent}
+    ]
   },
   {
     path:'home/:id',
